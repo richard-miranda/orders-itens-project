3a86a1a37ecd30654bfd7754b8d77555ca254870
Richard Miranda <richardmmianda@outlook.com>
2024-08-06 22:10:33 -0300

    Initializing project on repo

---
+++ order-itens-project/Entities/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace order_itens_project.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ order-itens-project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace order_itens_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
